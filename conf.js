5d2707179432021c2d5a89aff55c049ff8d280af
Chandrakanth Penta <CPenta@India.Telstrat.com>
2018-10-09 14:48:41 +0530

    added beautiful reporter function in conf.js

--- conf.js
+++ conf.js
@@ -1,8 +1,16 @@
+var HtmlReporter = require('protractor-beautiful-reporter');
+
+
 exports.config = {
 
     seleniumAddress: 'http://localhost:4444/wd/hub',
     specs: ['spec.js']
 
-    
+    onPrepare: function() {
+        
+        jasmine.getEnv().addReporter(new HtmlReporter({
+           baseDirectory: 'Reports/screenshots'
+        }).getJasmine2Reporter());
+     }
     
 };
